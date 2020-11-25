36f797822c1e3e114cc48c40c2acd81d2181244e
Eby-Samuel693 <coquelet.samuel@gmail.com>
2020-11-25 14:23:03 +0100

    ezf

---
+++ add.js
@@ -0,0 +1,26 @@
+let objet1 = prompt("quel element numérique utilisez-vous ?");
+
+switch(objet1) {
+    case 'sourie':
+        alert("vous utilisé une sourie");
+        break;
+    case 'clavier':
+        alert("vous utilisé un clavier");
+        break;
+    case 'ordinateur':
+        alert("vous utilisé un ordinateur");
+        break;
+    case 'tapis':
+        alert("vous utilisé un tapis");
+        break;
+    case 'imprimante':
+        alert("vous utilisé une imprimante");
+        break;
+    case 'onduleur':
+        alert("vous utilisé un onduleur");
+        break;
+    default :
+        alert("bon ok")
+}
+
+alert(objet1);
